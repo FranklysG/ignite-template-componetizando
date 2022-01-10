43b683cb69208151a1dccbcf559b5b56c5dfd3ed
FranklysG <tunele095@gmail.com>
2022-01-09 21:33:24 -0300

    remove this extra code in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import { useEffect, useState } from 'react';
-
 import { SideBar } from './components/SideBar';
 
 import './styles/global.scss';
 
-
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
-
 export function App() {
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
-
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <SideBar />
